e6bbd474d9640cf0105e83f37af4128baa567deb
Atul Raj <atulsid151@gmail.com>
2020-09-01 21:01:45 +0530

    Updates

---
+++ precache-manifest.dc5afec76b8111d3bd6ae8f238f23159.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cde5449c8c724f77d6f323d47883f69",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "2f238f37f1611287c2a6",
+    "url": "/ipod/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "5b84b9e3d81973c7025b",
+    "url": "/ipod/static/css/main.b8e1f11d.chunk.css"
+  },
+  {
+    "revision": "2f238f37f1611287c2a6",
+    "url": "/ipod/static/js/2.c0590a5b.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/ipod/static/js/2.c0590a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b84b9e3d81973c7025b",
+    "url": "/ipod/static/js/main.63474706.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  },
+  {
+    "revision": "673c9fa20d658fd2667468c7c6d63fe0",
+    "url": "/ipod/static/media/dum_dee_dum.673c9fa2.mp3"
+  }
+]);
